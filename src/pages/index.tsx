19667c95b75a65825731590f124112ad43e241dc
Naridz <naris2001@gmail.com>
2025-06-10 23:01:12 +0700

    ...

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import Searh from "@/components/Searh";
+import Head from "next/head";
 
 export default function Home() {
 
   return (
-    <div className="bg-radial-[at_25%_25%] from-white to-zinc-900 to-75% min-h-screen flex justify-center items-center">
-      <Searh/>
-    </div>
+    <>
+    <Head>
+      <title>Weather App</title>
+    </Head>
+      <div className="bg-radial-[at_25%_25%] from-white to-zinc-900 to-75% min-h-screen flex justify-center items-center">
+        <Searh/>
+      </div>
+    </>
   );
 }
